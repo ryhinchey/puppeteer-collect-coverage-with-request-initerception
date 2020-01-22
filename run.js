817894e2f3191001b23cd3235e74f3e5cc7c1317
Ryan Hinchey <rlhinchey@gmail.com>
2020-01-21 21:13:41 -0500

    move setRequestInterception to below calls to startCoverage

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setRequestInterception(true);
+
   page.on("request", request => {
     request.continue();
   });
@@ -12,6 +12,8 @@ const puppeteer = require("puppeteer");
   await page.coverage.startJSCoverage();
   await page.coverage.startCSSCoverage();
   console.log("finished startCoverage");
+
+  await page.setRequestInterception(true);
 
   await page.goto("https://google.com");
   console.log(await page.title());
